e0af4d047a5717682bb16a113336447a10bfcfaf
Yishan Wen <wittywys@gmail.com>
2020-01-19 15:33:12 +0800

    check filed mistake

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ rootNode.textContent = 'Hello World';
 fetch('/api/msg')
   .then(res => res.json())
   .then(body => {
-    if (body.data) {
+    if (body.text) {
       const node = document.createElement('p');
       node.textContent = 'Get value from api: ' + body.text;
       rootNode.appendChild(node);
